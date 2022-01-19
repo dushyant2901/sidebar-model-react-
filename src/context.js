0ebe2df21f083a7a59cff86d7fb9e61a82d1b03f
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-20 01:08:59 +0530

    final commit

--- src/context.js
+++ src/context.js
@@ -1,1 +1,24 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+import { createContext } from "react";
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const openSideBar = () => {
+    setIsSidebarOpen(true);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSidebarOpen(false);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return <AppContext.Provider value={{isModalOpen,openModal,closeModal,isSidebarOpen,openSideBar,closeSideBar}}>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => useContext(AppContext);
